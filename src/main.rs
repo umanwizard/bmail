037d7f9310e52584d5ceeb126d533a30c5117965
Brennan Vincent <brennan@umanwizard.com>
2021-08-14 10:22:18 -0400

    improve test script

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
+use bmail::error::EmailError;
 use bmail::headers::mime::ContentType;
 use bmail::headers::HeaderFieldInner;
 use bmail::parse::email::message;
+
+use nom::error::Error as NomError;
+use nom::Err as NomErr;
 
 use std::env;
 
@@ -10,7 +14,13 @@ fn main() {
     for f in args.skip(1) {
         println!("{}", f);
         let data = std::fs::read(f).unwrap();
-        let (_, message) = nom::combinator::complete(message())(&data).unwrap();
+        let (_, message) = match nom::combinator::complete(message())(&data) {
+            Ok(ok) => ok,
+            Err(NomErr::Error(EmailError::Parse(NomError { input, .. }, _))) => {
+                panic!("Error at: {}", String::from_utf8_lossy(input));
+            }
+            Err(e) => panic!("{:?}", e),
+        };
         println!("{:?}", message);
     }
 }
